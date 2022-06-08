c586c8aff16dbfa5f391939b840ff5199b61a2ce
Temasan <savin_ar@mail.ru>
2022-06-08 19:27:40 +0300

    working solution

--- PackageSolution/headers/PackageWays.hpp
+++ PackageSolution/headers/PackageWays.hpp
@@ -32,21 +32,40 @@ namespace PackageWays {
 
             putResult tryPutFloor(size_t level, Rectangle const &rect){
                 auto &floorFilled = m_filledByFloor[level];
+                // first floor case
                 if(floorFilled.empty()){
-                    size_t filledHeight = level == 0 ? 0 : m_filledByFloor[level - 1].begin()->getRightUpper().second ;
-                    if(filledHeight + rect.height() > m_height || rect.width() > m_width){
-                        return ERROR_IMPOSSIBLE;
+                    size_t upperLimit = m_height;
+                    size_t bottom_y = 0;
+                    // if levels already init on this level
+                    if(m_levelHeight.size() > level)
+                    {
+                        bottom_y = level > 0 ? m_levelHeight[level - 1] : 0;
+                        upperLimit = m_levelHeight[level];
                     }
-                    floorFilled.emplace_back(std::move(RectangleStoraged(std::move(std::make_pair(0, filledHeight)),
-                                                               std::move(std::make_pair(rect.width(), filledHeight + rect.height())), m_boxNumber)));
-                }
-                else {
-                    size_t filledWidth = floorFilled.rbegin()->getRightUpper().first;
-                    if(filledWidth + rect.width() > m_width){
+                    // new level data
+                    else if(m_levelHeight.size() + 1 > level && !m_levelHeight.empty()){
+                        bottom_y = level > 0 ? m_levelHeight[level - 1] : 0;
+                    }
+
+                    size_t rectUpperY = bottom_y + rect.height();
+                    if(rectUpperY > upperLimit || rect.width() > m_width){
+                        if(upperLimit == m_height)
+                            return ERROR_IMPOSSIBLE;
                         return ERROR_FILLED;
                     }
-                    std::pair<size_t, size_t> leftBottom = std::make_pair(filledWidth, floorFilled.rbegin()->getLeftBottom().second);
-                    std::pair<size_t, size_t> rightUpper = std::make_pair(filledWidth + rect.width(), floorFilled.rbegin()->getLeftBottom().second + rect.height());
+                    floorFilled.emplace_back(std::move(RectangleStoraged(std::move(std::make_pair(0, bottom_y)),
+                                                               std::move(std::make_pair(rect.width(), rectUpperY)), m_boxNumber)));
+                    // init new level
+                    if(upperLimit == m_height)
+                        m_levelHeight.emplace_back(rectUpperY);
+                }
+                else {
+                    size_t left_x = floorFilled.rbegin()->getRightUpper().first;
+                    if(left_x + rect.width() > m_width){
+                        return ERROR_FILLED;
+                    }
+                    std::pair<size_t, size_t> leftBottom = std::make_pair(left_x, floorFilled.rbegin()->getLeftBottom().second);
+                    std::pair<size_t, size_t> rightUpper = std::make_pair(left_x + rect.width(), floorFilled.rbegin()->getLeftBottom().second + rect.height());
 
                     for(auto const &ceil: m_filledByCeil[level]){
                         if(isFloorCeilIntersect(leftBottom, rightUpper, ceil.getLeftBottom(), ceil.getRightUpper())){
@@ -56,30 +75,48 @@ namespace PackageWays {
                     floorFilled.emplace_back(std::move(RectangleStoraged(std::move(leftBottom), std::move(rightUpper), m_boxNumber)));
                 }
                 return ERROR_OK;
-
             }
+
             putResult tryPutCeil(size_t level, Rectangle const &rect){
                 auto &floorFilled = m_filledByFloor[level];
-                //if(floorFilled.empty()){
-                //    return ERROR_IMPOSSIBLE;
-                //}
                 auto &ceilFilled = m_filledByCeil[level];
+
                 Rectangle rotRect{std::move(rect.rotate())};
-                size_t filledWidth = ceilFilled.empty() ? m_width : ceilFilled.rbegin()->getLeftBottom().first;
-                if(filledWidth < rotRect.width()){
+                size_t right_x = ceilFilled.empty() ? m_width : ceilFilled.rbegin()->getLeftBottom().first;
+                if(right_x < rotRect.width()){
                     return ERROR_FILLED;
                 }
-                size_t upperPoint = floorFilled.empty() ? m_height : floorFilled.begin()->getRightUpper().second;
-                std::pair<size_t, size_t> rightUpper = std::make_pair(filledWidth,
-                                                                      upperPoint);
-                std::pair<size_t, size_t> leftBottom = std::make_pair(filledWidth - rotRect.width(),
-                                                                      upperPoint - rotRect.height());
+                size_t upperLimit = m_height;
+                size_t bottom_y = 0;
+                // if levels already init on this level
+                if(m_levelHeight.size() > level)
+                {
+                    bottom_y = level > 0 ? m_levelHeight[level - 1] : 0;
+                    upperLimit = m_levelHeight[level];
+                }
+                // new level data
+                else if(m_levelHeight.size() + 1 > level && !m_levelHeight.empty()){
+                    bottom_y = level > 0 ? m_levelHeight[level - 1] : 0;
+                }
+                if(bottom_y + rotRect.height() > upperLimit){
+                    if(upperLimit == m_height)
+                        return ERROR_IMPOSSIBLE;
+                    return ERROR_FILLED;
+                }
+                std::pair<size_t, size_t> leftBottom = std::make_pair(right_x - rotRect.width(),
+                                                                      bottom_y);
+                std::pair<size_t, size_t> rightUpper = std::make_pair(right_x,
+                                                                      bottom_y + rotRect.height());
                 for(auto const & floorRect: floorFilled){
                     if(isFloorCeilIntersect(floorRect.getLeftBottom(), floorRect.getRightUpper(), leftBottom, rightUpper)){
                         return ERROR_FILLED;
                     }
                 }
                 ceilFilled.emplace_back(std::move(RectangleStoraged(std::move(leftBottom), std::move(rightUpper), m_boxNumber)));
+                // init new level
+                if(upperLimit == m_height){
+                    m_levelHeight.emplace_back(upperLimit);
+                }
                 return ERROR_OK;
             }
 
@@ -91,19 +128,15 @@ namespace PackageWays {
                     storaged.insert(storaged.end(), it.second.begin(), it.second.end());
                 }
             }
+            bool isClear(){
+                return m_filledByFloor.empty() && m_filledByCeil.empty();
+            }
         private:
             inline bool isFloorCeilIntersect(std::pair<size_t, size_t> const &leftBottomFloor, std::pair<size_t, size_t> const &rightUpperFloor,
                                     std::pair<size_t, size_t> const &leftBottomCeil, std::pair<size_t, size_t> const &rightUpperCeil)
             {
-                //if(rightUpperFloor.second > leftBottomCeil.second){
-                //    return (leftBottomFloor.first < leftBottomCeil.first && rightUpperFloor.first > rightUpperCeil.first) ||
-                //        (leftBottomFloor.first > leftBottomCeil.first && leftBottomFloor.first < rightUpperCeil.first) ||
-                //        (rightUpperFloor.first > leftBottomCeil.first && leftBottomFloor.first < rightUpperCeil.first);
-                //}
-                if(((leftBottomCeil.first < leftBottomFloor.first && rightUpperCeil.first > leftBottomFloor.first)
-                    ||(leftBottomFloor.first < leftBottomCeil.first && rightUpperFloor.first > leftBottomCeil.first))
-                        && (leftBottomCeil.second < leftBottomFloor.second && leftBottomFloor.second > rightUpperCeil.second)
-                    || (leftBottomFloor.second < leftBottomCeil.second && leftBottomCeil.second > rightUpperFloor.second)){
+                if(std::min(rightUpperFloor.first, rightUpperCeil.first) - std::max(leftBottomFloor.first, leftBottomCeil.first) > 0
+                    && std::min(rightUpperFloor.second, rightUpperCeil.second) - std::max(leftBottomFloor.second, leftBottomCeil.second) > 0){
                     return true;
                 }
                 return false;
@@ -111,6 +144,7 @@ namespace PackageWays {
 
             std::map<size_t, std::vector<RectangleStoraged>> m_filledByFloor;
             std::map<size_t, std::vector<RectangleStoraged>> m_filledByCeil;
+            std::vector<size_t> m_levelHeight;
             size_t m_height;
             size_t m_width;
             size_t m_boxNumber;
@@ -137,24 +171,23 @@ namespace PackageWays {
                 if(fillFloor != FCNRBox::ERROR_OK) {
                     fillCeil = curBox.tryPutCeil(level, *rect);
                 }
-
                 if(fillFloor == FCNRBox::ERROR_OK ||
                     fillCeil == FCNRBox::ERROR_OK){
                     ++rect;
                     filledBoxSize = 0;
                     level = 0;
-                }
-                else if(fillFloor == FCNRBox::ERROR_IMPOSSIBLE){
-                    fillCeil = curBox.tryPutCeil(level + 1, *rect);
-                    if(fillCeil == FCNRBox::ERROR_FILLED){
-                        ++filledBoxSize;
-                    }
-                    else{
+                } else if(fillFloor == FCNRBox::ERROR_IMPOSSIBLE || fillCeil == FCNRBox::ERROR_IMPOSSIBLE){
+                    if(curBox.isClear()){
                         ++rect;
                         filledBoxSize = 0;
+                        level = 0;
                     }
-                    level = 0;
-                } else{
+                    else{
+                        ++filledBoxSize;
+                        level = 0;
+                    }
+                }
+                else{
                     ++level;
                 }
             }
